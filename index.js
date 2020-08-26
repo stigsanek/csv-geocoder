347ebd3318554e2ea479fb679683e2e67bfcaf42
stigsanek <stigsanek@yandex.ru>
2020-08-26 13:38:16 +0400

    Реализует логику общего процесса парсинга, геокодирования и записи

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const runParser = require('./modules/csv-parser');
+
+// Paths config
+const file = {
+  input: path.resolve(__dirname, 'csv/input.csv'),
+  output: path.resolve(__dirname, 'csv/otput.csv')
+};
+
+runParser({
+  inputSeparator: ';',
+  inputPath: file.input,
+  outputPath: file.output
+});
+
+
---
+++ modules/csv-parser.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const csvParser = require('csv-parser');
+const getCsvWriter = require('./csv-writer');
+const runGeocoder = require('./geocoder');
+
+const parserResults = [];
+
+// Runs all processes
+const runParser = ({ inputSeparator, inputPath, outputPath }) => {
+  console.log('Started parsing...');
+  const csvWriter = getCsvWriter(outputPath);
+
+  fs.createReadStream(inputPath)
+    .pipe(csvParser({ separator: inputSeparator }))
+    .on('data', (data) => parserResults.push(data))
+    .on('end', async () => {
+      console.log('Started geocoding...');
+
+      const records = await runGeocoder(parserResults);
+      csvWriter.writeRecords(records)
+        .then(() => {
+          console.log(`Successfully! Processed ${records.length} lines.`);
+        })
+        .catch((e) => console.log(`Error writing to file. ${e}`));
+    });
+};
+
+module.exports = runParser;
